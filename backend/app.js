d4a572d8dd5b1f6fc414fc44a0e3d476af784440
riandroraul <riandroraul@gmail.com>
2023-07-11 16:14:06 +0700

    first commit

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const chats = require("./data/data");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = require("express")();
+const dotenv = require("dotenv");
+
+dotenv.config();
+const port = process.env.PORT;
+// app.use(cors({ credentials: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+app.get("/", function (req, res, next) {
+  res.send("Api is running");
+});
+
+app.get("/api/chat", function (req, res, next) {
+  if (req.query.hasOwnProperty("id")) {
+    const singleChat = chats.find((chat) => chat._id === req.query.id);
+    res.send(singleChat);
+  } else {
+    res.send(chats);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
